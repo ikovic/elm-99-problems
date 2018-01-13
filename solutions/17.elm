be038a1a217e04e022f1b4673e9703fcd9960d6f
Ivan Ković <kovic.ivan@gmail.com>
2018-01-13 19:35:42 +0100

    Create 17.elm

---
+++ solutions/17.elm
@@ -0,0 +1,36 @@
+import Html exposing (text)
+import List 
+
+
+split : List a -> Int -> (List a, List a)
+split list count =
+    let
+      first = List.take count list
+      second = List.drop count list 
+     in
+    (first, second)
+
+
+main =
+    text
+        (if (test) then
+            "Your implementation passed all tests."
+         else
+            "Your implementation failed at least one test."
+        )
+
+
+test : Bool
+test =
+    List.all (\(result, expect) -> result == expect)
+        [ ( split (List.range 1 5) 0, ([], [1, 2, 3, 4, 5]) )
+        , ( split (List.range 1 5) 2, ([1, 2], [3, 4, 5]) )
+        , ( split (List.range 1 5) 3, ([1, 2, 3], [4, 5]) )
+        , ( split (List.range 1 5) 4, ([1, 2, 3, 4], [5]) )
+        , ( split (List.range 1 5) 5, ([1, 2, 3, 4, 5], []) )
+        , ( split (List.range 1 5) 6, ([1, 2, 3, 4, 5], []) )
+        , ( split (List.range 1 5) (-1), ([], [1, 2, 3, 4, 5]) )
+        ]
+        && List.all (\(result, expect) -> result == expect)
+            [ ( split [ "aab", "b", "c", "aa" ] 2, ([ "aab", "b"],["c", "aa" ]))
+            ]

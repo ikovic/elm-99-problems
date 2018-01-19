a74c30a2337272ab47598ec84ef0ded919eef214
Ivan Ković <kovic.ivan@gmail.com>
2018-01-19 22:34:03 +0100

    Create 31.elm

---
+++ solutions/31.elm
@@ -0,0 +1,54 @@
+import Html
+
+isPrime : Int -> Bool
+isPrime n =
+    if n < 2 then
+        False
+    else
+        eratos (abs n) (List.range 2 (n // 2))
+
+
+eratos : Int -> List Int -> Bool
+eratos n cs =
+    case cs of
+        [] ->
+            True
+        -- matches list of at least one item where xs is list and x is the first item
+        x :: xs ->
+            if n % x == 0 then
+                False
+            else
+                eratos n (List.filter (\y -> (y % x) /= 0) xs)
+
+main : Html.Html a
+main =
+    Html.text <|
+        case test of
+            0 ->
+                "Your implementation passed all tests."
+
+            1 ->
+                "Your implementation failed one test."
+
+            x ->
+                "Your implementation failed " ++ (toString x) ++ " tests."
+
+
+test : Int
+test =
+    List.length <|
+        List.filter (\( result, expect ) -> result /= expect)
+            [ ( isPrime 36, False )
+            , ( isPrime 10, False )
+            , ( isPrime -1, False )
+            , ( isPrime 1, False )
+            , ( isPrime 0, False )
+            , ( isPrime 120, False )
+            , ( isPrime 2, True )
+            , ( isPrime 23, True )
+            , ( isPrime 6000, False )
+            , ( isPrime 7919, True )
+            , ( isPrime 7911, False )
+            , ( isPrime 63247, True )
+            , ( isPrime 63249, False )
+            ]

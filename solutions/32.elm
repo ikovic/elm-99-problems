6e34559c37ff9f33c1e2f2dc5ab2faf1a77404e1
Ivan Ković <kovic.ivan@gmail.com>
2018-01-19 22:46:03 +0100

    Create 32.elm

---
+++ solutions/32.elm
@@ -0,0 +1,42 @@
+import Html
+import List
+import Maybe
+
+
+gcd : Int -> Int -> Int
+gcd a b =
+    let 
+        remainder = a % b
+    in
+        if remainder == 0 then abs(b)
+        else gcd b remainder
+
+
+main =
+    Html.text
+        (if (test) then
+            "Your implementation passed all tests."
+         else
+            "Your implementation failed at least one test."
+        )
+
+
+test : Bool
+test =
+    List.all (\( result, expect ) -> result == expect)
+        [ ( gcd 36 63, 9 )
+        , ( gcd 10 25, 5 )
+        , ( gcd 120 120, 120 )
+        , ( gcd 2 12, 2 )
+        , ( gcd 23 37, 1 )
+        , ( gcd 45 330, 15 )
+        , ( gcd 24528 65934, 6 )
+        , ( gcd 120 -120, 120 )
+        , ( gcd -2 12, 2 )
+        , ( gcd 37 23, 1 )
+        ]
+
+
+(..) : Int -> Int -> List Int
+(..) start end =
+    List.range start end

eff3d36493d6ba640fc2ecd34f05f3b9e2e1eb6a
Ivan Ković <kovic.ivan@gmail.com>
2018-01-19 22:49:56 +0100

    Create 33.elm

---
+++ solutions/33.elm
@@ -0,0 +1,42 @@
+import Html 
+import List
+
+gcd : Int -> Int -> Int
+gcd a b =
+    let 
+        remainder = a % b
+    in
+        if remainder == 0 then abs(b)
+        else gcd b remainder
+
+coprime :  Int -> Int -> Bool
+coprime a b = 
+    if gcd a b == 1 then True else False
+
+main =
+    Html.text
+        (if (test) then
+            "Your implementation passed all tests."
+         else
+            "Your implementation failed at least one test."
+        )
+
+
+test : Bool
+test =
+    List.all (\(result, expect) -> result == expect)
+        [ ( coprime 36 63, False )
+        , ( coprime 10 25, False )
+        , ( coprime 120 120, False )
+        , ( coprime 2 12, False )
+        , ( coprime 1313 1600, True)
+        , ( coprime 23 37, True )
+        , ( coprime 45 330, False)
+        , ( coprime 24528 65934, False)
+        , ( coprime 1600 1313, True)
+        , ( coprime -23 37, True )
+        , ( coprime 330 45, False)
+        , ( coprime -23 37, True )
+        , ( coprime -330 -45, False)
+        , ( coprime -24528 65934, False)
+        ]

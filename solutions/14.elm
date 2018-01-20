adfc4290a83f2c69ec0c962d82eaed1c478180e5
Ivan Ković <kovic.ivan@gmail.com>
2018-01-20 08:58:34 +0100

    Create 14.elm

---
+++ solutions/14.elm
@@ -0,0 +1,33 @@
+import Html exposing (text)
+import List
+
+
+duplicate : List a -> List a
+duplicate list =
+    case list of
+        [] ->
+            []
+        x::xs ->
+            x :: x :: duplicate(xs)
+
+
+main =
+    text
+        (if (test) then
+            "Your implementation passed all tests."
+         else
+            "Your implementation failed at least one test."
+        )
+
+
+test : Bool
+test =
+    List.all (\(result, expect) -> result == expect)
+        [ ( duplicate [1, 2, 3, 5, 8, 8], [1, 1, 2, 2, 3, 3, 5, 5, 8, 8, 8, 8])
+        , ( duplicate [], [])
+        , ( duplicate [1], [1, 1])
+        ]
+        && List.all (\(result, expect) -> result == expect)
+            [ ( duplicate ["1", "2", "5"], 
+                ["1", "1", "2", "2", "5", "5"] )
+            ]

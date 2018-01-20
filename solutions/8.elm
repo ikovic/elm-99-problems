11e917514a514a7bf2f78438a7490cf3e56f6af2
Ivan Ković <kovic.ivan@gmail.com>
2018-01-20 19:41:19 +0100

    Create 8.elm

---
+++ solutions/8.elm
@@ -0,0 +1,48 @@
+import Html
+import List
+import Maybe
+
+noDupes : List a -> List a
+noDupes xs =
+    List.foldr noDupCons [] xs
+
+
+noDupCons : a -> List a -> List a
+noDupCons x xs =
+    case List.head xs of
+        Nothing -> [x]
+        Just a  -> 
+            if x == a then
+                xs
+            else
+                x :: xs
+                
+main : Html.Html a
+main =
+    Html.text
+        <| case test of
+            0 ->
+                "Your implementation passed all tests."
+
+            1 ->
+                "Your implementation failed one test."
+
+            x ->
+                "Your implementation failed " ++ (toString x) ++ " tests."
+
+
+test : Int
+test =
+    List.length
+        <| List.filter ((==) False)
+            [ noDupes [ 1, 1, 1, 1, 2, 5, 5, 2, 1 ] == [ 1, 2, 5, 2, 1 ]
+            , noDupes [ 2, 1, 1, 1 ] == [ 2, 1 ]
+            , noDupes [ 2, 2, 2, 1, 1, 1 ] == [ 2, 1 ]
+            , noDupes [ 1 ] == [ 1 ]
+            , noDupes [] == []
+            , noDupes [ "aa", "aa", "aa" ] == [ "aa" ]
+            , noDupes [ "aab", "b", "b", "aa" ] == [ "aab", "b", "aa" ]
+            ]
+
+
+      

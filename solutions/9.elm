5a40f00e1c005f8242730b91f8b7d2eca066a6bf
Ivan Ković <kovic.ivan@gmail.com>
2018-01-28 12:34:26 +0100

    Create 9.elm

---
+++ solutions/9.elm
@@ -0,0 +1,51 @@
+import Html
+import List
+import Maybe
+
+
+pack : List a -> List (List a)
+pack list =
+    List.foldr fPack [] list
+
+fPack : a ->  List (List a) -> List (List a)
+fPack x runs = 
+    case runs of 
+        [] -> [[x]]
+
+        y::ys ->
+            case List.head y of
+                Nothing -> []
+
+                Just rh ->
+                    if rh == x then
+                        (x :: y) :: ys
+                    else 
+                        [x] :: runs
+
+
+main : Html.Html a
+main =
+    Html.text
+        <| case test of
+            0 ->
+                "Your implementation passed all tests."
+
+            1 ->
+                "Your implementation failed one test."
+
+            x ->
+                "Your implementation failed " ++ (toString x) ++ " tests."
+
+
+test : Int
+test =
+    List.length
+        <| List.filter ((==) False)
+            [ pack [ 1, 1, 1, 1, 2, 5, 5, 2, 1 ] == [ [ 1, 1, 1, 1 ], [ 2 ], [ 5, 5 ], [ 2 ], [ 1 ] ]
+            , pack [ 2, 1, 1, 1 ] == [ [ 2 ], [ 1, 1, 1 ] ]
+            , pack [ 2, 2, 2, 1, 1, 1 ] == [ [ 2, 2, 2 ], [ 1, 1, 1 ] ]
+            , pack [ 1 ] == [ [ 1 ] ]
+            , pack [] == []
+            , pack [ "aa", "aa", "aa" ] == [ [ "aa", "aa", "aa" ] ]
+            , pack [ "aab", "b", "b", "aa" ] == [ [ "aab" ], [ "b", "b" ], [ "aa" ] ]
+            ]
